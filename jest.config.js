111467d0ee13c819dbf40c1fd0cfbfd1d08d39e0
hozayves <muhozayves2014@gmail.com>
2024-04-15 22:58:52 +0200

    [finishes #187429587] configure logger using winston & express-winston :fire: :fire:

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+const config = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+};
+exports.default = config;

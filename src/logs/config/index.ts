8c6f9eb36f9dae3e4b3af868d516db3d7fa44c89
hozayves <muhozayves2014@gmail.com>
2024-04-16 19:09:46 +0200

    [finishes #(187429587] Configure winstons logging library

---
+++ src/logs/config/index.ts
@@ -0,0 +1,82 @@
+import { Logger, createLogger, format, transports } from 'winston';
+
+interface HttpRequest {
+  originalUrl: string;
+  url: string;
+  method: string;
+  query: any;
+  headers: any;
+}
+// Function to format HTTP request information
+const formatHttpRequest = ({ originalUrl, url, method, query, headers }: HttpRequest) => {
+  return `originalUrl: ${originalUrl}, url: ${url}, method: ${method}, query: ${JSON.stringify(query)}, headers: ${JSON.stringify(headers)}`;
+};
+
+// Custom format function to handle HTTP request related information
+const httpRequestFormat = format.printf(({ timestamp, level, message, meta }) => {
+  if (meta && meta.req) {
+    const { originalUrl, url, method, query, headers } = meta.req;
+    return `${timestamp} [${level}]: ${message}, ${formatHttpRequest({ originalUrl, url, method, query, headers })}`;
+  } else {
+    return `${timestamp} [${level}]: ${message}`;
+  }
+});
+
+// Function to apply custom format function to all transports
+const applyCustomFormat = (loggerInstance: Logger) => {
+  loggerInstance.transports.forEach(transport => {
+    if (transport.format) {
+      transport.format = format.combine(transport.format, httpRequestFormat);
+    } else {
+      transport.format = httpRequestFormat;
+    }
+  });
+};
+// Function to create a transports with a common configuration
+const createCommonTransport = (filename: string, level: string) => {
+  return new transports.File({
+    filename,
+    level,
+    format: format.combine(format.timestamp({ format: 'DD/MM/YYYY - HH:mm:ss' }), format.json()),
+  });
+};
+
+// Create a logger instance
+const logger = createLogger({
+  transports: [
+    new transports.Console({
+      level: 'info',
+      format: format.combine(
+        format.colorize(),
+        format.timestamp({ format: 'DD/MM/YYYY - HH:mm:ss' }),
+        format.printf(({ timestamp, level, message, meta }) => {
+          const httpRequest = meta?.req;
+          if (httpRequest) {
+            const { originalUrl, url, method, query, headers } = httpRequest;
+            return `${timestamp} [${level}]: ${message}, originalUrl: ${originalUrl}, url: ${url}, method: ${method}, query: ${JSON.stringify(query)}, headers: ${JSON.stringify(headers)}`;
+          }
+          return `${timestamp} [${level}]: ${message}`;
+        })
+      ),
+    }),
+    createCommonTransport('./src/logs/system.log', 'info'),
+  ],
+});
+// Apply custom format function to all transports
+applyCustomFormat(logger);
+
+// Create an error logger instance
+export const errorLogger = createLogger({
+  transports: [
+    new transports.Console({
+      level: 'error',
+      format: format.combine(format.colorize(), format.json(), format.timestamp({ format: 'DD/MM/YYYY - HH:mm:ss' })),
+    }),
+    createCommonTransport('./src/logs/error.log', 'error'),
+  ],
+});
+
+// Apply custom format function to all error logger transports
+applyCustomFormat(errorLogger);
+
+export default logger;
--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,44 @@
 import type { Application, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import express from 'express';
-import YAML from 'yamljs';
-import swaggerUI from 'swagger-ui-express';
-import swaggerJsDoc from 'swagger-jsdoc';
-import options from './docs/swaggerdocs';
 
-const swaggerJsDocs = YAML.load('./src/docs/doc.yaml');
+import logger, { errorLogger } from './logs/config';
+import expressWinston from 'express-winston';
 
 dotenv.config();
 const app: Application = express();
 const PORT = process.env.PORT ?? 3000;
 
+/**
+ * By placing express-winston middleware before defining routes,
+ * we ensure that all HTTP requests and responses are logged
+ * before they are handled by the application's routes.
+ * This allows us to capture comprehensive logs of incoming
+ * requests and outgoing responses for monitoring and debugging purposes. */
+app.use(
+  expressWinston.logger({
+    winstonInstance: logger,
+    statusLevels: true,
+  })
+);
+
 app.get('/', (req: Request, res: Response): void => {
+  logger.error('Testing logger');
   res.send('Welcome at Mavericks E-commerce Website Apis');
 });
 
+/**
+ * By placing express-winston middleware after defining routes,
+ * we ensure that it captures any errors that occur during route handling.
+ * This allows us to log detailed error information, including stack traces,
+ * status codes, and error messages, for effective debugging and monitoring.
+ */
+app.use(
+  expressWinston.errorLogger({
+    winstonInstance: errorLogger,
+  })
+);
+
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  logger.info(`Server is running on port ${PORT}`);
 });
-
-const swaggerSpec = swaggerJsDoc(options);
-
-app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));

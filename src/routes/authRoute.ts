a515a03736fce9f206b63eaf6d7098a90363ed2c
hozayves <muhozayves2014@gmail.com>
2024-05-01 18:30:03 +0200

    ft-2FA two factor authentication for seller

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -1,21 +1,14 @@
 import { Router } from 'express';
 import passport from 'passport';
-import { authenticateViaGoogle } from '../controllers/authController';
+import { authenticateViaGoogle, verifyOTP } from '../controllers/authController';
 import { login } from '../controllers/authController';
+import { isCheckedOTP } from '../middlewares/otpAuthMiddleware';
 
 const router = Router();
 // redirect user to google for authentication
-router.get(
-  '/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email'],
-  })
-);
-
-// authenticated user and store user data
+router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 router.get('/google/callback', authenticateViaGoogle);
-
-// Route to login a user
 router.post('/login', login);
+router.post('/:token/otp', isCheckedOTP, verifyOTP);
 
 export default router;

4500dfd5cbe3860adf140e4b119ffdf02a3eaf71
NPL Codes <104311071+amin-leon@users.noreply.github.com>
2024-05-09 09:10:40 +0200

    [Finsishes #187354256] Marking product as available or unavailable

--- src/controllers/productsController.ts
+++ src/controllers/productsController.ts
@@ -4,6 +4,7 @@ import uploadImage from '../helpers/claudinary';
 import { sendInternalErrorResponse } from '../validations';
 import { Product, ProductAttributes } from '../database/models/Product';
 import { Size, SizeAttributes } from '../database/models/Size';
+import logger from '../logs/config';
 
 export const createProduct = async (req: Request, res: Response) => {
   try {
@@ -66,6 +67,90 @@ export const createSize = async (req: Request, res: Response) => {
       message: 'Product size added successfully',
     });
   } catch (error) {
+    sendInternalErrorResponse(res, error);
+  }
+};
+
+const checkSizeExistance = async (sizeId: string, res: Response) => {
+  const isSizeExist = await Size.findByPk(sizeId);
+
+  if (!isSizeExist) {
+    res.status(404).json({
+      ok: false,
+      message: 'This size is not available',
+    });
+    return null;
+  }
+  return isSizeExist;
+};
+
+const markProduct = async (sideId: string, availability: boolean, res: Response, message: string) => {
+  await Size.update(
+    {
+      available: availability,
+    },
+    {
+      where: {
+        id: sideId,
+      },
+    }
+  );
+
+  res.status(200).json({
+    ok: true,
+    message: message,
+  });
+};
+
+// Mark product as unvaiable
+export const markProductAsUnavailable = async (req: Request, res: Response) => {
+  try {
+    const { sizeId } = req.params;
+    const size = await checkSizeExistance(sizeId, res);
+
+    if (!size) {
+      return;
+    }
+
+    const isExpired = size.expiryDate <= new Date();
+    const isQuantityAvailable = size.quantity <= 0;
+
+    if (isExpired || isQuantityAvailable) {
+      await markProduct(sizeId, false, res, 'This Product is successfully marked as unavailable');
+    } else {
+      res.status(403).json({
+        ok: false,
+        message: 'This Product already exist in stock with valid expiration date',
+      });
+    }
+  } catch (error) {
+    logger.error(error);
+    sendInternalErrorResponse(res, error);
+  }
+};
+
+export const markProductAsAvailable = async (req: Request, res: Response) => {
+  try {
+    const { sizeId } = req.params;
+    const size = await checkSizeExistance(sizeId, res);
+
+    if (!size) {
+      return;
+    }
+
+    const isExpired = size.expiryDate > new Date();
+    const isQuantityAvailable = size.quantity >= 1;
+
+    if (isExpired && isQuantityAvailable) {
+      await markProduct(sizeId, true, res, 'This Product is successfully marked as available');
+    } else {
+      res.status(403).json({
+        ok: false,
+        message: 'Check This Product quanity if it exists in stock with valid expiration date',
+      });
+    }
+  } catch (error) {
+    logger.error(error);
     sendInternalErrorResponse(res, error);
   }
 };

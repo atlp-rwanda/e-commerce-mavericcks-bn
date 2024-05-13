d54033a1282950075aa88940709d06060f264ea5
JeanIrad <jado.milton@gmail.com>
2024-05-13 20:59:16 +0200

    [finishes #187585696] ordermanagement
    some changes
    ii
    [finishes #187354263]crud operations of items to a cart
    
    	new file:   src/controllers/cartController.ts
    	new file:   src/database/migrations/20240510102608-create-user-product.js
    	new file:   src/database/models/userProduct.ts
    
    	new file:   src/docs/cart.yaml
    	new file:   src/routes/cartRoute.ts
    	modified:   src/routes/index.ts
    
        pick a62f333 [finishes #187354263]crud operations of items to a cart
    
    fixing bugs
    squashing previous commits
    
    	new file:   .vscode/launch.json
    	modified:   package.json
    	new file:   src/controllers/cartController.ts
    	new file:   src/controllers/orderController.ts
    	modified:   src/controllers/productsController.ts
    	modified:   src/controllers/roleControllers.ts
    	modified:   src/dat
    
    abase/migrations/20240413203456-create-role.js
    	new file:   src/database/migrations/20240510102608-create-user-product.js
    	new file:   src/database/migrations/20240513164738-cart-model.js
    	new file:   src/database/migrations/20240514160800-orders.js
    	new file:   src/database/migrations/20240514174638-cart-product.js
    	modified:   src/database/models/Product.ts
    	new file:   src/database/models/cart.ts
    	new file:   src/database/models/order.ts
    	modified:   src/database/models/role.ts
    	modified:   src/database/seeders/20240427082911-create-default-role.js
    :i`	modified:   src/database/seeders/20240429200629-add-seller-role.js
    	new file:   src/docs/cart.yaml
    	modified:   src/middlewares/authMiddlewares.ts
    	new file:   src/routes/cartRoute.ts
    	modified:   src/routes/index.ts
    	new file:   src/routes/orderRoute.ts
    	modified:   src/routes/productRoutes.ts
    	modified:   src/validations/index.ts
    
    	new file:   .vscode/launch.json
    	modified:   package.json
    	new file:   src/controllers/cartController.ts
    	new file:   src/controllers/orderController.ts
    	modified:   src/controllers/productsController.ts
    	modified:   src/controllers/roleControllers.ts
    	new file:   src/database/migrations/20240510102608-create-user-product.js
    	new file:   src/database/migrations/20240513164738-cart-model.js
    	new file:   src/database/migrations/20240514160800-orders.js
    	new file:   src/database/migrations/20240514174638-cart-product.js
    	modified:   src/database/models/Product.ts
    	new file:   src/database/models/cart.ts
    	modified:   src/database/models/order.ts
    	modified:   src/database/models/role.ts
    	new file:   src/database/seeders/20240429200629-add-seller-role.js
    	new file:   src/docs/cart.yaml
    	new file:   src/docs/orders.yaml
    	modified:   src/middlewares/authMiddlewares.ts
    	new file:   src/routes/cartRoute.ts
    	modified:   src/routes/index.ts
    	new file:   src/routes/orderRoute.ts	modified:   src/routes/productRoutes.ts
    	modified:   src/validations/index.ts

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -2,6 +2,7 @@
 import passport from 'passport';
 import { Strategy as GoogleStrategy, StrategyOptionsWithRequest } from 'passport-google-oauth20';
 import User, { UserAttributes } from '../database/models/user';
+import getDefaultRole from '../helpers/defaultRoleGenerator';
 
 // Defining options required for Google OAuth 2.0 authentication strategy
 const googleStrategyOptions: StrategyOptionsWithRequest = {
@@ -41,6 +42,7 @@ passport.use(
         verified: true,
         createdAt: new Date(),
         updatedAt: new Date(),
+        RoleId: await getDefaultRole(),
       };
 
       const newUser = await User.create(newUserAttributes as UserAttributes);

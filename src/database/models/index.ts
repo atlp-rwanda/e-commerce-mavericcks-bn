7d4f2ba02b551e35b2b58c9beb60afe8a9c8177a
patrickhag <patrickhag09@gmail.com>
2024-06-26 16:00:14 +0200

    Adding test test to products controller

--- src/database/models/index.ts
+++ src/database/models/index.ts
@@ -1,7 +1,7 @@
 'use strict';
 
 import { Sequelize, Dialect } from 'sequelize';
-const env: string = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV as string;
 
 const config = require('../config/config.js');
 
